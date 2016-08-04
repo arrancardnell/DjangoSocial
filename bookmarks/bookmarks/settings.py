d0be5e5dcd346377a539d21f70eefcebe22c9af0
arrancardnell <arrancardnell@gmail.com>
2016-08-04 23:36:04 +0100

    Added redis for counting and ranking.

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -37,6 +37,10 @@ THUMBNAIL_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
+
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail',
                                         args=[u.username])
